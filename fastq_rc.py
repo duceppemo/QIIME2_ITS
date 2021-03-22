7f1ed6c871c8b2822a12224ed61c619ec520b83e
duceppemo <17859096+duceppemo@users.noreply.github.com>
2021-03-22 17:50:40 -0400

    Initial commit

---
+++ fastq_rc.py
@@ -0,0 +1,182 @@
+#!/usr/local/env python3
+
+import os
+import gzip
+import pathlib
+from argparse import ArgumentParser
+from concurrent import futures
+from multiprocessing import cpu_count
+
+
+__author__ = 'duceppemo'
+__version__ = '0.1'
+
+
+class FastqRC(object):
+    complement = {
+        'A': 'T',
+        'T': 'A',
+        'C': 'G',
+        'G': 'C',
+        "N": 'N'
+    }
+
+    def __init__(self, args):
+        # Get options
+        self.args = args
+        self.input_folder = args.input
+        self.output_folder = args.output
+        self.cpu = args.threads
+
+        # Dictionary to store all the fastq file(s)
+        self.master_dict = dict()
+
+        # Run
+        self.run()
+
+    def run(self):
+        """
+        This is where all the main program parts are being called
+        :return:
+        """
+        # Run all checks to make sure the script runs smoothly
+        self.checks()
+        # Create a list of all the fastq files in the input folder
+        fastq_list = FastqRC.list_fastq(self.input_folder)
+
+        # Parallel process fastq files
+        print('Processing:')
+        FastqRC.rc_fastq_parallel(fastq_list, self.output_folder, self.cpu)
+
+        # # Process each fastq file
+        # print('Processing:')
+        # for fq in fastq_list:
+        #     FastqRC.rc_fastq(fq, self.output_folder)
+
+    def checks(self):
+        """
+        Check if all options have been filled properly
+        :return:
+        """
+        # Check input folder
+        if not os.path.exists(self.input_folder):
+            raise Exception('Input folder does not exists')
+        if not os.path.isdir(self.input_folder):
+            raise Exception('Input is not a folder')
+
+        # Check output folder
+        if self.output_folder == self.input_folder:
+            raise Exception('Please choose a different output folder than the input folder')
+        if not os.path.exists(self.output_folder):
+            FastqRC.make_folder(self.output_folder)  # Create output folder is does not exists already
+
+    @staticmethod
+    def list_fastq(my_path):
+        """
+        Walk input directory and list all the fastq files. Accepted file extensions are '.fastq', '.fastq.gz',
+        '.fq' and '.fq.gz'.
+        :param my_path: string. Input folder path
+        :return: list of strings. Fastq files in input folder
+        """
+        # Create empty list to hold the file paths
+        fastq_list = list()
+        # Walk the input directory recursively and look for fastq files
+        for root, directories, filenames in os.walk(my_path):
+            for filename in filenames:
+                absolute_path = os.path.join(root, filename)
+                if os.path.isfile(absolute_path) and filename.endswith(('.fastq', '.fastq.gz', '.fq', '.fq.gz')):
+                    fastq_list.append(absolute_path)  # Add fastq file path to the list
+        return fastq_list
+
+    @staticmethod
+    def rc_fastq_parallel(fastq_list, out_folder, ncpu):
+        """
+        Parallel process the fastq files
+        :param fastq_list: string. List of fastq file paths
+        :param out_folder: string. Output folder path
+        :param ncpu: int. Number of files to process in parallel
+        :return:
+        """
+        with futures.ThreadPoolExecutor(max_workers=ncpu) as executor:
+            args = ((fastq, out_folder) for fastq in fastq_list)
+            for results in executor.map(lambda p: FastqRC.rc_fastq(*p), args):  # (*p) does the unpacking part
+                pass
+
+    @staticmethod
+    def rc_fastq(fastq, out_folder):
+        """
+        Parse fastq file into a dictionary
+        :param fastq: string. Input fastq file path
+        :param out_folder: string. Output folder path
+        :return:
+        """
+        print('\t{}'.format(os.path.basename(fastq)))
+
+        # Create file path of output file
+        outfile = out_folder + '/' + os.path.basename(fastq)
+        # Since all output files are gzipped, make sure to add ".gz" extensions to the input files
+        # that were not compressed
+        if not outfile.endswith('.gz'):
+            outfile += '.gz'
+
+        with gzip.open(outfile, 'wb') as out_f:
+            with gzip.open(fastq, 'rb') if fastq.endswith('.gz') else open(fastq, 'r') as in_f:
+                counter = 0
+                for line in in_f:
+                    counter += 1
+                    if fastq.endswith('.gz'):
+                        line = line.decode('ascii')  # Convert to ASCII if files were gzipped (binary)
+                    line = line.rstrip()
+
+                    if counter == 1:  # Header line line
+                        if not line.startswith('@'):  # first line of 4 should always start with "@"
+                            raise Exception('Fastq file invalid.')
+                        out_f.write('{}\n'.format(line).encode())
+                    elif counter == 2:  # Sequence line
+                        line = line[::-1]  # reverse
+                        seq_rc = ''
+                        for bp in line:
+                            seq_rc += FastqRC.complement[bp]
+                        out_f.write('{}\n'.format(seq_rc).encode())
+                    elif counter == 3:  # that's the "+" line, we can skip it
+                        out_f.write('{}\n'.format(line).encode())
+                    elif counter == 4:  # Quality line
+                        out_f.write('{}\n'.format(line[::-1]).encode())  # reverse
+                        counter = 0  # Reset counter because each fastq entry in a file always have 4 lines
+
+    @staticmethod
+    def make_folder(folder):
+        """
+        Create output folder.
+        :param folder: string. Output folder path.
+        :return:
+        """
+        # Will create parent directories if don't exist and will not return error if already exists
+        pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+
+
+if __name__ == '__main__':
+    cpu = cpu_count()
+
+    parser = ArgumentParser(description='Reverse complement all entries in a fastq file.')
+    parser.add_argument('-i', '--input', metavar='/input_folder/',
+                        required=True,
+                        type=str,
+                        help='Input folder with fastq file(s), gzipped or not. '
+                             'Accepted file extensions are ".fastq", ".fastq.gz", ".fq" and ".fq.gz". '
+                             'Will look for files recursively. '
+                             'Mandatory.')
+    parser.add_argument('-o', '--output', metavar='/modified_fastq/',
+                        required=True,
+                        type=str,
+                        help='Output folder. Must be different than input folder. '
+                             'Mandatory.')
+    parser.add_argument('-t', '--threads', metavar='{}'.format(cpu),
+                        required=False, default=cpu,
+                        type=int,
+                        help='Number of CPU. Default is {}'.format(cpu))
+
+    # Get the arguments into an object
+    arguments = parser.parse_args()
+
+    FastqRC(arguments)
---
+++ train_unite_classifier_qiime2.py
@@ -0,0 +1,225 @@
+#!/usr/local/env python3
+
+import os
+import subprocess
+import pathlib
+import shutil
+import urllib.request
+import tarfile
+from argparse import ArgumentParser
+from multiprocessing import cpu_count
+
+
+class Qiime2Trainer(object):
+    def __init__(self, args):
+        self.args = args
+        self.url = args.url
+        self.output_folder = args.output_folder
+        self.qiime2_env = args.qiime2
+        self.cpu = args.threads
+
+        # Run
+        self.run()
+
+    def run(self):
+        self.checks()
+        tmp_file = self.output_folder + '/tmp.tar.gz'
+        print('Downloading UNITE databse')
+        # Qiime2Trainer.download(self.url, tmp_file)
+        (unite_seq, unite_taxo) = Qiime2Trainer.untargz(tmp_file)  # tuple
+        unite_seq_fixed = '.'.join(unite_seq.split('.')[:-1]) + '_upper.fasta'
+        print('Proofreading database...')
+        Qiime2Trainer.fix_fasta(unite_seq, unite_seq_fixed)
+
+        qiime2_seq = unite_seq_fixed + '.qza'
+        qiime2_taxo = unite_taxo + '.qza'
+
+        print('Preparing sequences for QIIME2...')
+        Qiime2Trainer.qiime2_import_unite_sequences(unite_seq_fixed, qiime2_seq)
+        print('Preparing taxonomy for QIIME2...')
+        Qiime2Trainer.qiime2_import_unite_taxonomy(unite_taxo, qiime2_taxo)
+
+        unite_version = os.path.basename(unite_seq).split("_")[3]
+        unite_clustering = os.path.basename(unite_seq).split("_")[4]
+        unite_release_date = os.path.basename(unite_seq).split("_")[5]
+        qiime2_classifier = '{}/unite-{}-{}-classifier-{}.qza'.format(self.output_folder, unite_version,
+                                                                      unite_clustering, unite_release_date)
+
+        print('Training UNITE classifier for QIIME2...')
+        Qiime2Trainer.qiime2_train_unite_classifier(qiime2_seq, qiime2_taxo, qiime2_classifier)
+
+        # Cleanup temporary files
+        print('Removing temporary files...')
+        pass
+
+        print('DONE')
+
+    def checks(self):
+        # Check output folder
+        if not os.path.exists(self.output_folder):
+            Qiime2Trainer.make_folder(self.output_folder)  # Create output folder is does not exists already
+
+        # Check if qiime2 conda environment is activated
+        if 'qiime2' not in os.environ['CONDA_DEFAULT_ENV']:
+            raise Exception('You must activate your QIIME2 conda environment to run this script. '
+                            '"conda activate qiime2-2020.8"')
+
+    @staticmethod
+    def make_folder(folder):
+        """
+        Create output folder.
+        :param folder: string. Output folder path.
+        :return:
+        """
+        # Will create parent directories if don't exist and will not return error if already exists
+        pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+
+    @staticmethod
+    def download(url, file_path):
+        """
+        Download the file from `url` and save it locally under `file_name`
+        https://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
+        https://unite.ut.ee/repository.php
+        :param url: string. URL of file (remotely)
+        :param file_path: string. Path of downloaded file (locally)
+        :return:
+        """
+        with open(file_path, 'wb') as out_file:
+            with urllib.request.urlopen(url) as response:
+                shutil.copyfileobj(response, out_file)
+
+    @staticmethod
+    def untargz(targz_file):
+        """
+        Decompress and return path of UNITE sequence and taxonomy files
+        :param targz_file:
+        :return: sting tuple. Path of sequence and taxonomy files
+        """
+        output_path = os.path.dirname(targz_file)
+
+        if targz_file.endswith('.tar.gz'):
+            with tarfile.open(targz_file, "r:gz") as f:  # Open for reading with gzip compression
+                f.extractall(path=output_path)
+
+        # Retrieve path of created directory
+        seq_file = ''
+        taxo_file = ''
+        for (root, dirs, files) in os.walk(output_path):
+            for name in files:
+                file_path = os.path.join(root, name)
+                if ('developer' in file_path) and ('sh_refs_qiime' in file_path) \
+                        and ('_99_' in file_path) and ('.fasta'in file_path):
+                    seq_file = file_path
+                elif ('developer' in file_path) and ('sh_taxonomy_qiime' in file_path) \
+                        and ('_99_' in file_path) and ('.txt'in file_path):
+                    taxo_file = file_path
+        return seq_file, taxo_file
+
+    @staticmethod
+    def fix_fasta(input_fasta, output_fasta):
+        """
+        Fix formatting errors that prevent importation of the reference sequences into QIIME2.
+        There are white spaces that interfere, and possibly some lower case letters that need
+        to be converted to upper case
+        :param input_fasta: sting. Path to input fasta file
+        :param output_fasta: sting. Path to modified fasta file
+        :return:
+        """
+        with open(output_fasta, 'w') as out_f:
+            with open(input_fasta, 'r') as in_f:
+                for line in in_f:
+                    line = line.rstrip().replace(' ', '')  # Remove trailing carriage return
+                    if not line.startswith('>'):
+                        line = line.upper()  # To upper case and remove space
+                    out_f.write('{}\n'.format(line))
+
+    @staticmethod
+    def qiime2_import_unite_sequences(sequence_file, qiime2_sequence_file):
+        """
+
+        :param sequence_file:
+        :param qiime2_sequence_file:
+        :return:
+        """
+        cmd = ['qiime', 'tools', 'import',
+               '--type', 'FeatureData[Sequence]',
+               '--input-path', sequence_file,
+               '--output-path', qiime2_sequence_file]
+
+        subprocess.run(cmd)
+
+    @staticmethod
+    def qiime2_import_unite_taxonomy(taxonomy_file, qiime2_taxonomy_file):
+        """
+
+        :param taxonomy_file:
+        :param qiime2_taxonomy_file:
+        :return:
+        """
+        cmd = ['qiime', 'tools', 'import',
+               '--type', 'FeatureData[Taxonomy]',
+               '--input-format', 'HeaderlessTSVTaxonomyFormat',
+               '--input-path', taxonomy_file,
+               '--output-path', qiime2_taxonomy_file]
+        subprocess.run(cmd)
+
+    @staticmethod
+    def qiime2_train_unite_classifier(sequence_file, taxonomy_file, classifier_file):
+        """
+        # Training the QIIME2 Classifier with UNITE ITS Reference Sequences
+        # http://john-quensen.com/tutorials/training-the-qiime2-classifier-with-unite-its-reference-sequences/
+        :param sequence_file: string. QIIME2 sequence file (.qza)
+        :param taxonomy_file: string. QIIME2 taxonomy file (.qza)
+        :param classifier_file: string. QIIME2 classifier file (.qza)
+        :return:
+        """
+        cmd = ['qiime', 'feature-classifier', 'fit-classifier-naive-bayes',
+               '--i-reference-reads', sequence_file,
+               '--i-reference-taxonomy', taxonomy_file,
+               '--o-classifier', classifier_file]
+
+        subprocess.run(cmd)
+
+
+if __name__ == '__main__':
+    cpu = cpu_count()
+
+    parser = ArgumentParser(description='Train Unite classifier for QIIME2')
+    parser.add_argument('-u', '--url', metavar='http://www.fileserver.com/file.txt',
+                        required=True,
+                        type=str,
+                        help='URL of UNITE database (compressed tar file with sequences and taxonomy)'
+                             'Mandatory.')
+    parser.add_argument('-o', '--output_folder', metavar='/unite_folder/',
+                        required=True,
+                        type=str,
+                        help='Output folder for classifier.'
+                             'Mandatory.')
+    parser.add_argument('-q', '--qiime2', metavar='qiime2-2020.8',
+                        required=True,
+                        type=str,
+                        help='Name of your QIIME2 conda environment'
+                             'Mandatory.')
+    parser.add_argument('-t', '--threads', metavar='{}'.format(cpu),
+                        required=False, default=cpu,
+                        type=int,
+                        help='Number of CPU. Default is {}'.format(cpu))
+
+    # Get the arguments into an object
+    arguments = parser.parse_args()
+
+    Qiime2Trainer(arguments)
+
+
+"""
+#!/bin/bash
+
+####
+# Train Classifier
+####
+
+
+
+
+
+"""
