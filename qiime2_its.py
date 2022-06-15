e3da1a2d8e1521097f04adc6764984da44f3f6b5
Marc-Olivier Duceppe <17859096+duceppemo@users.noreply.github.com>
2022-06-15 10:02:27 -0400

    Update qiime2_its.py

--- qiime2_its.py
+++ qiime2_its.py
@@ -86,12 +86,12 @@ class Qiime2(object):
             Qiime2Methods.make_folder(itsxpress_log_folder)
 
             # Extract Fungi ITS1 in parallel
-            # if self.single:
-            #     Qiime2Methods.extract_its_se_parallel(self.fastq_list, its_folder, itsxpress_log_folder,
-            #                                           self.cpu, self.parallel, self.taxa, self.region)
-            # else:  # if self.paired:
-            #     Qiime2Methods.extract_its_pe_parallel(self.sample_dict, its_folder, itsxpress_log_folder,
-            #                                           self.cpu, self.parallel, self.taxa, self.region)
+            if self.single:
+                Qiime2Methods.extract_its_se_parallel(self.fastq_list, its_folder, itsxpress_log_folder,
+                                                      self.cpu, self.parallel, self.taxa, self.region)
+            else:  # if self.paired:
+                Qiime2Methods.extract_its_pe_parallel(self.sample_dict, its_folder, itsxpress_log_folder,
+                                                      self.cpu, self.parallel, self.taxa, self.region)
 
             # Remove empty sequences. This is an artifact from ITSxpress.
             print('Checking for empty entries...')
