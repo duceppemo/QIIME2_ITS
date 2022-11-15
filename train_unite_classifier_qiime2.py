8961374892810c1d25b552909e5aab6cf3e05792
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-15 05:17:09 +0000

    Adding tarfile member sanitization to extractall()

--- train_unite_classifier_qiime2.py
+++ train_unite_classifier_qiime2.py
@@ -89,7 +89,26 @@ class Qiime2Trainer(object):
 
         if targz_file.endswith('.tar.gz'):
             with tarfile.open(targz_file, "r:gz") as f:  # Open for reading with gzip compression
-                f.extractall(path=output_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, path=output_path)
 
         # Retrieve path of created directory
         seq_file = ''
